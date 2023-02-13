5acac90ea621986c7a5e44b22f34b5c2f1746916
jainil9 <122803607+jainil9@users.noreply.github.com>
2023-02-13 11:06:01 -0500

    Add files via upload

---
+++ StausUser.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+/**
+ * MidTerm-Exam
+ * @author srinivsi
+ */
+public class StausUser 
+{
+   public void statusDetail(String code)
+{
+switch(code.toUpperCase())
+        {
+        case "ZERO": System.out.println("User Status REJECTED");
+        break;
+        case "ONE": System.out.println("User Status PENDING");
+        break;
+        case "TWO":
+        System.out.println("User Status PROCESSING");
+        break;
+        case "THREE": 
+            System.out.println("User Status APPROVED");
+        break;
+        default:
+         System.out.println("NOT VALID CODE");
+        break;
+        }
+}
+ 
+}
